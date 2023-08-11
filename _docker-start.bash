eb4288f2e3f48c283fedcb3d534b0c154b250915
Alexandre Nozière <anoziere@openstudio.fr>
2023-08-11 14:26:28 +0200

    add spx options + wip node (disable atm)

--- _docker-start.bash
+++ _docker-start.bash
@@ -1,72 +1,93 @@
 #!/bin/bash
+############################################################
+# Help                                                     #
+############################################################
+Help() {
+  # Display Help
+  echo "This script will build and run docker containers for Thelia 2.5."
+  echo
+  echo "Syntax: _docker-start.bash [--xdebug|spx]"
+  echo "arguments:"
+  echo "--xdebug     will build php with xdebug"
+  echo "--spx     will build php with spx"
+  exit
+}
 
+with_xdebug=0
+with_spx=0
+while [ -n "$1" ]; do
+  case "$1" in
+  	--xdebug) with_xdebug=1 ;;
+  	--spx) with_spx=1 ;;
+  	help) Help ;;
+    *) echo "Option $1 not recognized" ;;
+  	esac
+  	shift
+done
+
+# CREATE ENV FILE
 if ! test -f ".env"; then
   read -p "$(echo -e "\e[1;37;45m You don't have a .env file, we will create it. Please enter a template name : \e[0m")" template_name
   if [[ -z "$template_name" ]]; then
     echo -e "\e[1;37;41m Invalid template name \e[0m"
     exit 0
   fi
-  # first, strip underscores
   template_name=${template_name//_/}
-  # next, replace spaces with underscores
   template_name=${template_name// /_}
-  # now, clean out anything that's not alphanumeric or an underscore
   template_name=${template_name//[^a-zA-Z0-9_]/}
-  # finally, lowercase with TR
   template_name="$(echo $template_name | tr '[A-Z]' '[a-z]')"
   cp ".env.docker" ".env"
   sed -i "s/modern/${template_name}/g" .env >/dev/null
   echo -e "\e[1;37;42m .env file created with success with template name \"${template_name}\" \e[0m"
 fi
 
-
-if [ ! -f Thelia ]
-then
-  read -p "$(echo -e "\e[1;37;45m Do you want to enable php xdebug ? [Y/n] : \e[0m")" with_xdebug
-  with_xdebug=${with_xdebug:-'n'}
-  export WITH_XDEBUG=with_xdebug
-  echo -e "\e[1;37;46m Starting docker \e[0m"
-
-  docker-compose up -d --build php-fpm
-  docker-compose exec php-fpm composer --no-interaction create-project thelia/thelia-project thelia 2.5.*
-  rm -Rf thelia/.docker
-  # TODO enable
-  #cp thelia/.gitignore .gitignore
-  mv -vn thelia/{.,}* ./
-  rm -Rf thelia
-  rm -f templates/frontOffice/modern/.dockerignore
-else
-  docker-compose up -d php-fpm
-fi
-
+# GET ENV VARIABLES
 set -o allexport
 eval $(cat '.env' | sed -e '/^#/d;/^\s*$/d' -e 's/\(\w*\)[ \t]*=[ \t]*\(.*\)/\1=\2/' -e "s/=['\"]\(.*\)['\"]/=\1/g" -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
 set +o allexport
 
-if [ ! -z "$ACTIVE_FRONT_TEMPLATE" ] && [ ! -d "templates/frontOffice/$ACTIVE_FRONT_TEMPLATE" ]; then
-  echo -e "\e[1;37;46m Copying template files to templates/frontOffice/$ACTIVE_FRONT_TEMPLATE \e[0m"
-  cp -r "templates/frontOffice/modern" "templates/frontOffice/$ACTIVE_FRONT_TEMPLATE"
-else
-  echo "Template files "$template_name" already exists"
-fi
+# INSTALL AND / OR START
+if [ ! -f Thelia ]; then
+  export WITH_XDEBUG=$with_xdebug
+  export WITH_SPX=$with_spx
+  # INSTALL THELIA PROJECT
+  docker-compose up -d --build php-fpm
+  docker-compose exec php-fpm composer --no-interaction create-project thelia/thelia-project thelia 2.5.*
+  rm -Rf thelia/.docker
+  mv -vn thelia/{.,}* ./
+  rm -Rf thelia
+  rm -f templates/frontOffice/modern/.dockerignore
 
-
-if [ ! -f Thelia ]
-then
+  # START SERVICES
   docker-compose up -d --build webserver
   docker-compose up -d --build mariadb
-  docker-compose up -d --build node
-  sh .docker/php-fpm/docker-init.sh
+  docker-compose up -d --build mailhog
+  #docker-compose up -d --build node
+
+  # INSTALL THELIA AND MODERN THEME
+  if [[ $ACTIVE_FRONT_TEMPLATE == 'modern' ]]; then
+    echo -e 'Install thelia and init theme modern'
+    chmod +x .docker/php-fpm/docker-init-modern.sh
+    sh .docker/php-fpm/docker-init-modern.sh
+  else
+    echo -e 'Install thelia'
+    docker-compose exec php-fpm php Thelia thelia:install --db_host=mariadb --db_port=3306 --db_username=root --db_name="${MYSQL_DATABASE}" --db_password="${MYSQL_ROOT_PASSWORD}"
+  fi
 else
+  # ONLY START SERVICES
+  docker-compose up -d php-fpm
   docker-compose up -d webserver
   docker-compose up -d mariadb
-  docker-compose up -d node
+  docker-compose up -d mailhog
+  #docker-compose up -d node
 fi
 
+# DEMO DATAS
 if [[ $1 = "-demo" ]]; then
   docker-compose exec php-fpm php local/setup/import.php
 fi
 
-docker-compose exec php-fpm php Thelia c:c
-docker-compose exec php-fpm php Thelia c:c --env=prod
-docker-compose exec php-fpm php Thelia c:c --env=propel
+# CACHE
+docker-compose exec php-fpm php Thelia cache:clear
+docker-compose exec php-fpm php Thelia cache:clear --env=prod
+docker-compose exec php-fpm php Thelia cache:clear --env=propel
